a2db4094195280ba33ecd3259d0f0091243b3937
Syunyo Kawamoto <syunyo1028@yahoo.co.jp>
2020-01-27 21:32:15 +0900

    init

---
+++ app/main.py
@@ -0,0 +1,49 @@
+import os
+import datetime
+import remoclient
+import spreadsheet
+
+
+def run():
+    gs = spreadsheet.SpreadSheet()
+    gs.open_sheet(filename=os.environ['SHEET_NAME'])
+    last_line = gs.get_col_length()
+    if last_line == 0:
+        header = [
+            'timestamp',
+            'timestamp_unix',
+            'temperature',
+            'temperature_created_at',
+            'humidity',
+            'humidity_created_at',
+            'illumination',
+            'illumination_created_at',
+            'motion',
+            'motion_created_at'
+        ]
+        gs.update_column(1, header)
+        last_line += 1
+
+    now = datetime.datetime.now(datetime.timezone.utc)
+    client = remoclient.NatureRemoClient()
+    events = client.get_newest_events()
+    now_iso = now.isoformat()
+
+    values = [
+        now_iso,
+        int(now.timestamp()),
+        events['te']['val'],
+        events['te']['created_at'],
+        events['hu']['val'],
+        events['hu']['created_at'],
+        events['il']['val'],
+        events['il']['created_at'],
+        events['mo']['val'],
+        events['mo']['created_at']
+    ]
+    gs.update_column(last_line + 1, values)
+    print('done!')
+
+
+if __name__ == "__main__":
+    run()
---
+++ app/spreadsheet.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+class SpreadSheet(object):
+
+    def __init__(self):
+        scope = ['https://spreadsheets.google.com/feeds',
+                 'https://www.googleapis.com/auth/drive']
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            os.environ['GS_CREDENTIAL_PATH'], scope)
+        self.gs = gspread.authorize(credentials)
+
+    def open_sheet(self, filename):
+        self.sheet = self.gs.open(filename).sheet1
+
+    def get_col_length(self, row=1):
+        return len(self.sheet.col_values(row))
+
+    def update_column(self, line, values):
+        cells = self.sheet.range(line, 1, line, len(values))
+        for i in range(len(cells)):
+            cells[i].value = values[i]
+            self.sheet.update_cells(cells)
